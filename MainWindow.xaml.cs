8e69f4f1ef9d38996970b41ebfb2add56bc4de6e
Andrew Toto <totoan5@yahoo.com>
2026-04-01 20:32:09 -0500

    Created WPF files and proto app window

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace DashboardApp;
+public partial class MainWindow : Window
+{
+    private CpuCalculator cpu;
+    private GpuCalculator gpu;
+    private RamCalculator ram;
+    private NetworkCalculator net;
+    private DispatcherTimer timer;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        cpu = new CpuCalculator();
+        gpu = new GpuCalculator();
+        ram = new RamCalculator();
+        net = new NetworkCalculator();
+
+        timer = new DispatcherTimer();
+        timer.Interval = TimeSpan.FromSeconds(1);
+        timer.Tick += Timer_Tick;
+        timer.Start();
+    }
+
+    private void Timer_Tick(object? sender, EventArgs e)
+    {
+        float cpuusage = cpu.GetCpuUsage();
+        float gpuusage = gpu.GetGpuUsage();
+        float ramusage = ram.GetRamUsage();
+        var netusage = net.GetNetworkUsage();
+
+        CpuText.Text = $"CPU: {cpuusage}%";
+        GpuText.Text = $"GPU: {gpuusage}%";
+        RamText.Text = $"RAM: {ramusage}%";
+        NetText.Text = $"Network In: {Math.Round(netusage.In/1000)}KB/s | Network Out: {Math.Round(netusage.Out/1000)}KB/s";
+    }
+}
--- UsageCalculator.cs
+++ UsageCalculator.cs
@@ -1,130 +1,133 @@
 ﻿using System.Diagnostics;
 
-GpuCalculator gpu = new GpuCalculator();
-CpuCalculator cpu = new CpuCalculator();
-RamCalculator ram = new RamCalculator();
-NetworkCalculator net = new NetworkCalculator();
+// GpuCalculator gpu = new GpuCalculator();
+// CpuCalculator cpu = new CpuCalculator();
+// RamCalculator ram = new RamCalculator();
+// NetworkCalculator net = new NetworkCalculator();
 
-while (true)
+// while (true)
+// {
+//     float cpuusage = cpu.GetCpuUsage();
+//     float gpuusage = gpu.GetGpuUsage();
+//     float ramusage = ram.GetRamUsage();
+//     var netusage = net.GetNetworkUsage();
+
+//     Console.Clear();
+//     Console.WriteLine("CPU:" + cpuusage + "%");
+//     Console.WriteLine("GPU:" + gpuusage + "%");
+//     Console.WriteLine("RAM:" + ramusage + "%");
+//     Console.WriteLine("Network In:" + Math.Round(netusage.In/1000) + "KB/s | Network Out:" + Math.Round(netusage.Out/1000) + "KB/s");
+
+//     Thread.Sleep(1000);
+// }
+
+namespace DashboardApp
 {
-    float cpuusage = cpu.GetCpuUsage();
-    float gpuusage = gpu.GetGpuUsage();
-    float ramusage = ram.GetRamUsage();
-    var netusage = net.GetNetworkUsage();
-
-    Console.Clear();
-    Console.WriteLine("CPU:" + cpuusage + "%");
-    Console.WriteLine("GPU:" + gpuusage + "%");
-    Console.WriteLine("RAM:" + ramusage + "%");
-    Console.WriteLine("Network In:" + Math.Round(netusage.In/1000) + "KB/s | Network Out:" + Math.Round(netusage.Out/1000) + "KB/s");
-
-    Thread.Sleep(1000);
-}
-
-public class CpuCalculator
-{
-    private PerformanceCounter counter;
-     
-    public CpuCalculator()
+    public class CpuCalculator
     {
-        counter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
-        counter.NextValue();
-
-        Thread.Sleep(1000);
-    }
-
-    public float GetCpuUsage()
-    {
-        float usage;
+        private PerformanceCounter counter;
         
-        usage = counter.NextValue();
-
-        return usage;
-    }
-}
-
-public class GpuCalculator
-{
-    private PerformanceCounterCategory gpuCounterCat;
-    private List<PerformanceCounter> gpuCountersList;
-
-    public GpuCalculator()
-    {
-        gpuCounterCat = new PerformanceCounterCategory("GPU Engine");
-        gpuCountersList = new List<PerformanceCounter>();
-
-        string[] gpuInstanceNames = gpuCounterCat.GetInstanceNames();
-    
-        foreach (string name in gpuInstanceNames)
+        public CpuCalculator()
         {
-            PerformanceCounter gpuCounter = new PerformanceCounter("GPU Engine", "Utilization Percentage", name);
-            gpuCountersList.Add(gpuCounter);
-        }
-
-        foreach (PerformanceCounter counter in gpuCountersList)
-        {
+            counter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
             counter.NextValue();
+
+            // Thread.Sleep(1000);
         }
 
-        Thread.Sleep(1000);
-    }
-
-    public float GetGpuUsage()
-    {
-        float total = 0;
-
-        foreach (PerformanceCounter counter in gpuCountersList)
+        public float GetCpuUsage()
         {
-            total += counter.NextValue();
+            float usage;
+            
+            usage = counter.NextValue();
+
+            return usage;
+        }
+    }
+
+    public class GpuCalculator
+    {
+        private PerformanceCounterCategory gpuCounterCat;
+        private List<PerformanceCounter> gpuCountersList;
+
+        public GpuCalculator()
+        {
+            gpuCounterCat = new PerformanceCounterCategory("GPU Engine");
+            gpuCountersList = new List<PerformanceCounter>();
+
+            string[] gpuInstanceNames = gpuCounterCat.GetInstanceNames();
+        
+            foreach (string name in gpuInstanceNames)
+            {
+                PerformanceCounter gpuCounter = new PerformanceCounter("GPU Engine", "Utilization Percentage", name);
+                gpuCountersList.Add(gpuCounter);
+            }
+
+            foreach (PerformanceCounter counter in gpuCountersList)
+            {
+                counter.NextValue();
+            }
+
+            // Thread.Sleep(1000);
         }
 
-        return total;
-    }
-}
+        public float GetGpuUsage()
+        {
+            float total = 0;
 
-public class RamCalculator
-{
-    private PerformanceCounter counter;
+            foreach (PerformanceCounter counter in gpuCountersList)
+            {
+                total += counter.NextValue();
+            }
 
-    public RamCalculator()
-    {
-        counter = new PerformanceCounter("Memory", "% Committed Bytes In Use");
-        counter.NextValue();
-
-        Thread.Sleep(1000);
+            return total;
+        }
     }
 
-    public float GetRamUsage()
+    public class RamCalculator
     {
-        float usage;
+        private PerformanceCounter counter;
 
-        usage = counter.NextValue();
+        public RamCalculator()
+        {
+            counter = new PerformanceCounter("Memory", "% Committed Bytes In Use");
+            counter.NextValue();
 
-        return usage;
-    }
-}
+            // Thread.Sleep(1000);
+        }
 
-public class NetworkCalculator
-{
-    private PerformanceCounter incounter;
-    private PerformanceCounter outcounter;
+        public float GetRamUsage()
+        {
+            float usage;
 
-    public NetworkCalculator()
-    {
-        incounter = new PerformanceCounter("Network Adapter", "Bytes Received/sec", "realtek pcie 2.5gbe family controller");
-        outcounter = new PerformanceCounter("Network Adapter", "Bytes Sent/sec", "realtek pcie 2.5gbe family controller");
+            usage = counter.NextValue();
 
-        incounter.NextValue();
-        outcounter.NextValue();
-
-        Thread.Sleep(1000);
+            return usage;
+        }
     }
 
-    public (float In, float Out) GetNetworkUsage()
+    public class NetworkCalculator
     {
-        float usagein = incounter.NextValue();
-        float usageout = outcounter.NextValue();
+        private PerformanceCounter incounter;
+        private PerformanceCounter outcounter;
 
-        return (usagein, usageout);
+        public NetworkCalculator()
+        {
+            incounter = new PerformanceCounter("Network Adapter", "Bytes Received/sec", "realtek pcie 2.5gbe family controller");
+            outcounter = new PerformanceCounter("Network Adapter", "Bytes Sent/sec", "realtek pcie 2.5gbe family controller");
+
+            incounter.NextValue();
+            outcounter.NextValue();
+
+            // Thread.Sleep(1000);
+        }
+
+        public (float In, float Out) GetNetworkUsage()
+        {
+            float usagein = incounter.NextValue();
+            float usageout = outcounter.NextValue();
+
+            return (usagein, usageout);
+        }
     }
 }
